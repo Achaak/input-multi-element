7ba9e45e0cd1539594e22d755d41cf9c6003bc40
Achak <axel.lavoie@gmail.com>
2019-06-18 17:31:51 +0200

    Correction

--- README.md
+++ README.md
@@ -1,6 +1,228 @@
-# input-multiple-element
+# input-multi-element
+This is a module to simply add a input with multiple element to your site.
 
-This framework makes it easy to create an input with several elements.
+## Requirement :
+* JQuery
 
-Requires : 
-  - jquery
+## Install :
+Load the required files.
+Inside the page's head tag include the input-multi-element's CSS file.
+``` html
+<link rel="stylesheet" href="/your-path/input-multi-element.css">
+```
+
+And in the page's footer, include the required javascript files.
+``` html
+<script src="/your-path/input-multi-element.js"></script>
+```
+
+## Initialize :
+For initialize the input-multi-element, insert this example snippet and update this with your parameters.
+Exemple: 
+``` js
+$(document).ready(function() {
+    InputMultiElement().init(
+        "#input-multi-element",
+        {
+            maxElement: 50
+        }
+    );
+});
+```
+
+## Fonctions :
+- [inputMultiElement().init](#init)
+- [inputMultiElement().setElement](#setElement)
+- [inputMultiElement().setElements](#setElements)
+- [inputMultiElement().getElements](#getElements)
+- [inputMultiElement().getElementsDOM](#getElementsDOM)
+- [inputMultiElement().getElementById](#getElementById)
+- [inputMultiElement().getElementDOMById](#getElementDOMById)
+- [inputMultiElement().getNbElements](#getNbElements)
+- [inputMultiElement().deleteLastElement](#deleteLastElement)
+- [inputMultiElement().deleteElementById](#deleteElementById)
+- [inputMultiElement().deleteElementsByValue](#deleteElementsByValue)
+- [inputMultiElement().updateElementsById](#updateElementsById)
+- [inputMultiElement().updateElementsByValue](#updateElementsByValue)
+- [inputMultiElement().hasElement](#hasElement)
+- [inputMultiElement().nbElementByValue](#nbElementByValue)
+- [inputMultiElement().activateTextZone](#activateTextZone)
+- [inputMultiElement().clearTextZone](#clearTextZone)
+
+
+
+
+__inputMultiElement().init__ <a name="init"></a>
+``` js
+/**
+ * Used to create the input mutli element
+ *
+ * @param {string} _elem Class or id of the input mutli element
+ * @param {Array} _opts List of options
+ */
+inputMultiElement.init(_elem, _opts)
+```
+
+__inputMultiElement().setElement__ <a name="setElement"></a>
+``` js
+/**
+ * Used to set a element with a index or not
+ *
+ * @param {string} _text Text of the element
+ * @param {number} _index Index of the element
+ */
+inputMultiElement.setElement(_text, _index)
+```
+
+__inputMultiElement().setElements__ <a name="setElements"></a>
+``` js
+/**
+ * Used to set a array of elements with a index or not
+ *
+ * @param {Array} _texts Array of the string elements
+ * @param {number} _index Index of the first element of the array
+ */
+inputMultiElement.setElements(_texts, _index)
+```
+
+__inputMultiElement().getElements__ <a name="getElements"></a>
+``` js
+/**
+ * Used to get an array of string of elements
+ *
+ * @returns {Array} Returns an array of elements
+ */
+inputMultiElement.getElements()
+```
+
+__inputMultiElement().getElementsDOM__ <a name="getElementsDOM"></a>
+``` js
+/**
+ * Used to get an array of DOM elements
+ *
+ * @returns {Array} Returns an array of DOM elements
+ */
+inputMultiElement.getElementsDOM()
+```
+
+__inputMultiElement().getElementById__ <a name="getElementById"></a>
+``` js
+/**
+ * Used to get an element with an index
+ *
+ * @param {number} _index Index of the element
+ * @returns {Array} Returns an element
+ */
+inputMultiElement.getElementById(_id)
+```
+
+__inputMultiElement().getElementDOMById__ <a name="getElementDOMById"></a>
+``` js
+/**
+ * Used to get an DOM element with an index
+ *
+ * @param {number} _index Index of the element
+ * @returns {Array} Returns a DOM element
+ */
+inputMultiElement.getElementDOMById(_id)
+```
+
+__inputMultiElement().getNbElements__ <a name="getNbElements"></a>
+``` js
+/**
+ * Used to get a number of the elements
+ *
+ * @returns {number} Returns number of elements
+ */
+inputMultiElement.getNbElements()
+```
+
+__inputMultiElement().deleteLastElement__ <a name="deleteLastElement"></a>
+``` js
+/**
+ * Used to delete the last element
+ */
+inputMultiElement.deleteLastElement()
+```
+
+__inputMultiElement().deleteElementById__ <a name="deleteElementById"></a>
+``` js
+/**
+ * Used to delete an element with a index
+ *
+ * @param {number} _index Index of the element
+ */
+inputMultiElement.deleteElementById(_id)
+```
+
+__inputMultiElement().deleteElementsByValue__ <a name="deleteElementsByValue"></a>
+``` js
+/**
+ * Used to delete an elements with a value
+ *
+ * @param {string} _values Value of the elements
+ */
+inputMultiElement.deleteElementsByValue(_value)
+```
+
+__inputMultiElement().updateElementsById__ <a name="updateElementsById"></a>
+``` js
+/**
+ * Used to update an elements with an index
+ *
+ * @param {string} _value Value of the elements
+ * @param {number} _index Index of the elements
+ */
+inputMultiElement.updateElementsById(_value, _id)
+```
+
+__inputMultiElement().updateElementsByValue__ <a name="updateElementsByValue"></a>
+``` js
+/**
+ * Used to update an elements with a value
+ *
+ * @param {string} _newValue New value of the elements
+ * @param {string} _lastValue Last value of the elements
+ */
+inputMultiElement.updateElementsByValue(_newValue, _lastValue)
+```
+
+__inputMultiElement().hasElement__ <a name="hasElement"></a>
+``` js
+/**
+     * Used to know if an element exist
+  *
+  * @param {string} _value Value of the elements
+  * @returns {boolean} Return true if the element exist if not false
+  */
+inputMultiElement.hasElement(_value)
+```
+
+__inputMultiElement().nbElementByValue__ <a name="nbElementByValue"></a>
+``` js
+/**
+ * Used to get a number of the elements with a value
+ *
+ * @param {string} _value Value of the elements
+ * @returns {number} Returns number of elements by value
+ */
+inputMultiElement.nbElementByValue(_value)
+```
+
+__inputMultiElement().activateTextZone__ <a name="activateTextZone"></a>
+``` js
+/**
+ * Used to activate the text zone
+ *
+ * @param {boolean} _bool True to activate / False to desactivate 
+ */
+inputMultiElement.activateTextZone(_bool)
+```
+
+__inputMultiElement().clearTextZone__ <a name="clearTextZone"></a>
+``` js
+/**
+ * Used to clear the text zone
+ */
+inputMultiElement.clearTextZone()
+```
--- input-multiple-element.js
+++ input-multiple-element.js
@@ -1,103 +1,571 @@
-(function($) {
-    $.fn.listElement = function(e) {
-        if(e) {
-            var nbMaxElem = (e.nbMaxElem ? e.nbMaxElem : 100000 );
-        }
+function InputMultiElement() {
+    var inputMultiElementDOM = undefined;
+    var textareaDOM          = $('<textarea class="ime-text-zone activate"></textarea>');
+    var opts = {
+        maxElement: 1000000
+    };
 
-        $(this).html(
-            "<textarea class='fw-textarea'></textarea>"
-        );
-        
-        var textarea = $('.fw-textarea', this);
-        var thisElem = $(this);
-        resizeTextArea();
 
-        $(textarea).focusout(function(e) { newElement(); });
 
-        $(textarea).keydown(function(e) {
-            if(e.keyCode == 13) {
-                e.preventDefault();
-                newElement(); 
+    /**
+     * Used to create the input mutli element
+     *
+     * @public
+     * @param {string} _elem Class or id of the input mutli element
+     * @param {Array} _opts List of options
+     */
+    function init(_elem, _opts) {
+        // Defind variable
+        inputMultiElementDOM = $(_elem);
+        inputMultiElementDOM.empty(); // Clear element DOM
+        inputMultiElementDOM.addClass("input-multi-element");
+        $.extend(opts, _opts);
+
+        // Add the textarea on the DOM
+        inputMultiElementDOM.append(textareaDOM);
+
+        // Bind this
+        bindInputMultiElement();
+    }
+
+
+    /**
+     * Used to create the input mutli element
+     *
+     * @private
+     * @param {string} _elem Class or id of the input mutli element
+     * @param {Array} _opts List of options
+     */
+    function bindInputMultiElement() {
+        // Add a new elemetn if the textarea is focus out
+        textareaDOM.focusout(function(_e) { 
+            // Get target
+            var _target = $(_e.currentTarget);
+
+            // Get value
+            var _value = _target.val();
+
+            // Verif if the value is not null
+            if (_value != "") {
+                // Add the new element
+                setElement(_value);
+
+                // Reset the textarea
+                clearTextZone()
             }
-            else if(e.keyCode == 8) {
-                if($(textarea).val() == '')
-                    deleteElement($(".fw-elem:nth-last-child(2)", thisElem));
-            }
-
-            resizeTextArea();
-        });
-
-        $(thisElem).click(function(e) {
-            $(textarea).focus();
-        });
-
-        $( window ).resize(function() {            
-            resizeTextArea();
-        });
-
-
-        function deleteElement(elem) {
-            $(elem).remove();
-
-            if($(".fw-elem", thisElem).length < nbMaxElem)
-                $(textarea).show();                
             
-            resizeTextArea();
-        }
+        });
 
-        function newElement() {
-            if($(textarea).val() != '') {
-                $("<div class='fw-elem'><div><span class='text'>"+$(textarea).val()+"</span><span class='delete'>×</span></div></div>").insertBefore(textarea);
-                $(textarea).val('');
+        // Add a new element if press enter in the textarea or delete if the textarea is empty and the backspace key is press
+        textareaDOM.keydown(function(_e) {
+            if(_e.keyCode == 13) { // Return
+                _e.preventDefault();
 
-                $('.delete', thisElem).click(function(e) { deleteElement($(this).parent().parent()); });
+                // Get target
+                var _target = $(_e.currentTarget);
+
+                // Get value
+                var _value = _target.val();
+
+                // Verif if the value is not null
+                if (_value != "") {
+                    // Add the new element
+                    setElement(_value);
+
+                    // Reset the textarea
+                    clearTextZone()
+                }
             }
+            else if(_e.keyCode == 8) { // Backspace
+                // Get target
+                var _target = $(_e.currentTarget);
 
-            if($(".fw-elem", thisElem).length >= nbMaxElem)
-                $(textarea).hide();
-
-            resizeTextArea();
-        }
-
-        function resizeTextArea() {
-            var thisElemWidth = $(thisElem).outerWidth();
-            var tmpElemsWidth = 0;
-            for (var i = 0; i < $(".fw-elem", thisElem).length; i++) {
-                var elemWidth = $('.fw-elem:nth-child('+(i+1)+')', thisElem).outerWidth();
-                if($(thisElem).width() < (tmpElemsWidth + elemWidth))
-                    tmpElemsWidth = 0;
-                if($(thisElem).width()-tmpElemsWidth < 30)
-                    tmpElemsWidth = 0;
+                // Get value
+                var _value = _target.val();
                 
-                if(elemWidth < $(thisElem).width())
-                    tmpElemsWidth += elemWidth;
-                else 
-                    tmpElemsWidth = 0;
+                // Verif if the value is null
+                if (_value == "") {
+                    // Delete the last element
+                    deleteLastElement();
+                }
             }
-            
-            $(textarea).outerWidth($(thisElem).width()-tmpElemsWidth);
+        });
+
+        // Focus if input mutli element is clicked
+        inputMultiElementDOM.click(function(_e) {
+            if (!$(_e.target).closest(".ime-elem").length)
+                textareaDOM.focus();
+        });
+    }
+
+
+
+    /**
+     * Used to clear the text zone
+     *
+     * @public
+     */
+    function clearTextZone() {
+        textareaDOM.val("");
+    }
+
+
+
+    /**
+     * Used to set a element with a index or not
+     *
+     * @public
+     * @param {string} _text Text of the element
+     * @param {number} _index Index of the element
+     */
+    function setElement(_text, _index) {
+        // Verif max element
+        if (getNbElements() < opts.maxElement) {
+
+            // Create the new element
+            var _newElement = $("<div class='ime-elem'><span class='ime-text'>"+_text+"</span><span class='ime-delete'>×</span></div>");
+
+            // Bind the cross
+            _newElement.find(".ime-delete").click(function(_e) {
+                // Get target
+                var _target = $(_e.currentTarget);
+
+                // Remove element
+                _target.closest(".ime-elem").remove();
+
+                onDeleteElement();
+            });
+
+            // Bind the text
+            _newElement.find(".ime-text").click(function(_e) {
+                if ($(_e.target).hasClass("ime-textarea-elem"))
+                    return false;
+
+                // Get target
+                var _target = $(_e.currentTarget);
+
+                // Get value
+                var _value = _target.text()
+
+                // Create the textarea
+                var _textareaElemDOM = $("<textarea class='ime-textarea-elem'>"+_value+"</textarea>");
+                
+                _target.html(_textareaElemDOM);
+
+                // Focus the textarea
+                _textareaElemDOM.select();
+
+
+
+                // Bind focus out
+                _textareaElemDOM.focusout(function(_e) {
+                    // Get target
+                    var _target = $(_e.currentTarget);
+
+                    // Get value
+                    var _value = _target.val();
+
+                    updateElementsById(_value, _target.closest(".ime-elem").index())
+                });
+
+
+                
+                // Bind focus out key enter
+                _textareaElemDOM.keydown(function(_e) {
+                    if(_e.keyCode == 13) { // Return
+                        _e.preventDefault();
+
+                        // Get target
+                        var _target = $(_e.currentTarget);
+    
+                        // Get value
+                        var _value = _target.val();
+    
+                        updateElementsById(_value, _target.closest(".ime-elem").index())
+                    }
+                });
+            });
+
+            // Insert the new element
+            if( !_index && _index != 0) { // If has not a index
+                _newElement.insertBefore(textareaDOM);
+            }
+            else { // If has a index
+                var _elementAfter = $(inputMultiElementDOM.find(".ime-elem")[_index]);
+                
+                if (_elementAfter.length) _newElement.insertBefore(_elementAfter);
+                else                      _newElement.insertBefore(textareaDOM);
+            }
+
+
+            // Event
+            onSetElement(_newElement);
         }
-    };
+    }
 
 
-    $.fn.getElements = function(e) {
-        var target = $(this)
-        var elements = [];
 
-        for (let index = 0; index < target.find(".text").length; index++) {
-            elements.push($(target.find(".text")[index]).html());
+    /**
+     * Used to set a array of elements with a index or not
+     *
+     * @public
+     * @param {Array} _texts Array of the string elements
+     * @param {number} _index Index of the first element of the array
+     */
+    function setElements(_texts, _index) {
+        for (var i = 0; i < _texts.length; i++) {
+            if( !_index && _index != 0) // If has not a index
+                setElement(_texts[i]);
+            else
+                setElement(_texts[i], (_index+i));
         }
-        return elements;
-    };
+    }
 
 
-    $.fn.addElements = function(element) {
-        var textarea = $('.fw-textarea', this);
+
+    /**
+     * Used to delete the last element
+     *
+     * @public
+     */
+    function deleteLastElement() {
+        // Get the last element
+        var _elem = inputMultiElementDOM.find('.ime-elem').last();
+
+        if (_elem.length) {
+            // Delete element
+            _elem.remove();
+
+            // Event
+            onDeleteElement();
+        }
+    }
+    
+    
+
+    /**
+     * Used to delete an element with a index
+     *
+     * @public
+     * @param {number} _index Index of the element
+     */
+    function deleteElementById(_index) {
+        // Get the last element
+        var _elem = inputMultiElementDOM.find('.ime-elem')[_index];
+
+        try {
+            // Delete element
+            _elem.remove();
+
+            // Event
+            onDeleteElement();
+        } catch(_e) {}
+    }
+
+
+
+    /**
+     * Used to delete an elements with a value
+     *
+     * @public
+     * @param {string} _values Value of the elements
+     */
+    function deleteElementsByValue(_value) {
+        for (var i = 0; i < inputMultiElementDOM.find(".ime-elem").length; i++) {
+            // Get element DOM
+            var _elemDOM = $(inputMultiElementDOM.find(".ime-elem")[i]);
+
+            if (_elemDOM.find(".ime-text").text() == _value)
+                deleteElementById(i);
+        }
+    }
+
+
+
+    /**
+     * Used to update an elements with an index
+     *
+     * @public
+     * @param {string} _value Value of the elements
+     * @param {number} _index Index of the elements
+     */
+    function updateElementsById(_value, _index) {
+        // Get element
+        var _elemDOM = $(inputMultiElementDOM.find('.ime-elem')[_index]);
+
+        if (_elemDOM.length) {
+            // Update element
+            _elemDOM.find('.ime-text').text(_value);
+
+            // Event
+            onUpdateElement(_elemDOM)
+        }
+    }
+
+
+
+    /**
+     * Used to update an elements with a value
+     *
+     * @public
+     * @param {string} _newValue New value of the elements
+     * @param {string} _lastValue Last value of the elements
+     */
+    function updateElementsByValue(_newValue, _lastValue) {
+        for (var i = 0; i < inputMultiElementDOM.find(".ime-elem").length; i++) {
+            // Get element DOM
+            var _elemDOM = $(inputMultiElementDOM.find(".ime-elem")[i]);
+
+            if (_elemDOM.find(".ime-text").text() == _lastValue) {
+                _elemDOM.find(".ime-text").text(_newValue)
+
+                // Event
+                onUpdateElement(_elemDOM)
+            }
+        }
+    }
+
+
+
+    /**
+     * Used to get an array of string of elements
+     *
+     * @public
+     * @returns {Array} Returns an array of elements
+     */
+    function getElements() {
+        var _elemList = [];
+
+        for (var i = 0; i < inputMultiElementDOM.find(".ime-elem").length; i++) {
+            // Get element DOM
+            var _elemDOM = $(inputMultiElementDOM.find(".ime-elem")[i]);
+
+            // Get element value
+            _elemList.push(_elemDOM.find(".ime-text").text());
+        }
+
+        return _elemList;
+    }
+
+
+
+    /**
+     * Used to get an array of DOM elements
+     *
+     * @public
+     * @returns {Array} Returns an array of DOM elements
+     */
+    function getElementsDOM() {
+        var _elemList = [];
+
+        for (var i = 0; i < inputMultiElementDOM.find(".ime-elem").length; i++) {
+            // Get element DOM
+            var _elemDOM = $(inputMultiElementDOM.find(".ime-elem")[i]);
+
+            // Get element value
+            _elemList.push(_elemDOM);
+        }
+
+        return _elemList;
+    }
+    
+    
+    
+    /**
+     * Used to get an element with an index
+     *
+     * @public
+     * @param {number} _index Index of the element
+     * @returns {Array} Returns an element
+     */
+    function getElementById(_index) {
+        // Get element DOM
+        var _elemDOM = $(inputMultiElementDOM.find(".ime-elem")[_index]);
+
+        if (_elemDOM.length)
+            return _elemDOM.find(".ime-text").text();
+    }
+    
+    
+    
+    /**
+     * Used to get an DOM element with an index
+     *
+     * @public
+     * @param {number} _index Index of the element
+     * @returns {Array} Returns a DOM element
+     */
+    function getElementDOMById(_index) {
+        // Get element DOM
+        var _elemDOM = $(inputMultiElementDOM.find(".ime-elem")[_index]);
+
+        if (_elemDOM.length)
+            return _elemDOM;
+    }
+
+
+
+    /**
+     * Used to get a number of the elements
+     *
+     * @public
+     * @returns {number} Returns number of elements
+     */
+    function getNbElements() {
+        return inputMultiElementDOM.find(".ime-elem").length;
+    }
+
+
+
+    /**
+     * Used to get a number of the elements with a value
+     *
+     * @public
+     * @param {string} _value Value of the elements
+     * @returns {number} Returns number of elements by value
+     */
+    function nbElementByValue(_value) {
+        var cptElement = 0;
+
+        for (var i = 0; i < inputMultiElementDOM.find(".ime-elem").length; i++) {
+            // Get element DOM
+            var _elemDOM = $(inputMultiElementDOM.find(".ime-elem")[i]);
+
+            if (_elemDOM.find(".ime-text").text() == _value) {
+                cptElement++;
+            }
+        }
+
+        return cptElement;
+    }
+
+
+
+    /**
+     * Used to know if an element exist
+     *
+     * @public
+     * @param {string} _value Value of the elements
+     * @returns {boolean} Return true if the element exist if not false
+     */
+    function hasElement(_value) {
+        return (nbElementByValue(_value) ? true : false);
+    }
+
+
+
+    /**
+     * Used to activate the text zone
+     *
+     * @public
+     * @param {boolean} _bool True to activate / False to desactivate 
+     */
+    function activateTextZone(_bool) {
+        textareaDOM[(_bool ? "addClass" : "removeClass")]("activate");
+    }
+
+
+
+    /**
+     * Used to trigger an event if an element is created
+     *
+     * @private
+     * @param {Array} _elem DOM of the element set
+     */
+    function onSetElement(_elem) {
+        inputMultiElementDOM.trigger("ime:element:add", {
+            element: _elem,
+            text: _elem.find(".ime-text").html(),
+            index: _elem.index()
+        });
         
-        for (let index = 0; index < element.length; index++) {
-            $("<div class='fw-elem'><div><span class='text'>"+element[index]+"</span><span class='delete'>×</span></div></div>").insertBefore(textarea);
+        // Desactivate textarea
+        if (getNbElements() >= opts.maxElement) activateTextZone(false);
+    }
 
-            $('.delete', this).click(function(e) { deleteElement($(this).parent().parent()); });
+
+
+    /**
+     * Used to trigger an event if an element is deleted
+     *
+     * @private
+     */
+    function onDeleteElement() {
+        inputMultiElementDOM.trigger("ime:element:delete");
+        
+        // Activate textarea
+        if (getNbElements() < opts.maxElement) activateTextZone(true);
+    }
+
+
+
+    /**
+     * Used to trigger an event if an element is updated
+     *
+     * @private
+     * @param {Array} _elem DOM of the element set
+     */
+    function onUpdateElement(_elem) {
+        inputMultiElementDOM.trigger("ime:element:update", {
+            element: _elem,
+            text: _elem.find(".ime-text").html(),
+            index: _elem.index()
+        });
+    }
+
+
+
+    return {
+        "init": function(_elem, _opts) {
+            init(_elem, _opts);
+        },
+        "setElement": function(_text, _index) {
+            setElement(_text, _index);
+        },
+        "setElements": function(_texts, _index) {
+            setElements(_texts, _index);
+        },
+        "getElements": function() {
+            return getElements();
+        },
+        "getElementsDOM": function() {
+            return getElementsDOM();
+        },
+        "getElementById": function(_id) {
+            return getElementById(_id);
+        },
+        "getElementDOMById": function(_id) {
+            return getElementDOMById(_id);
+        },
+        "getNbElements": function() {
+            return getNbElements();
+        },
+        "deleteLastElement": function() {
+            deleteLastElement();
+        },
+        "deleteElementById": function(_id) {
+            deleteElementById(_id);
+        },
+        "deleteElementsByValue": function(_value) {
+            deleteElementsByValue(_value);
+        },
+        "updateElementsById": function(_value, _id) {
+            updateElementsById(_value, _id);
+        },
+        "updateElementsByValue": function(_newValue, _lastValue) {
+            updateElementsByValue(_newValue, _lastValue);
+        },
+        "hasElement": function(_value) {
+            return hasElement(_value);
+        },
+        "nbElementByValue": function(_value) {
+            return nbElementByValue(_value);
+        },
+        "activateTextZone": function(_bool) {
+            return activateTextZone(_bool);
+        },
+        "clearTextZone": function() {
+            clearTextZone();
         }
     };
-})(jQuery); 
+}
